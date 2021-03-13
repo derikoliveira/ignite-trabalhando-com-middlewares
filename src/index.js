497091b63e84990cb86652dfa529ce9ed71528ae
Derik Oliveira <derikoliveira03@gmail.com>
2021-03-13 11:25:28 -0300

    Exercicio completo

--- src/index.js
+++ src/index.js
@@ -10,19 +10,76 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User account not found' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro) {
+    if (user.todos.length >= 10) {
+      return response.status(403).json({ errror: 'Free plan limit reached' });
+    }
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const validId = validate(id);
+
+  if (!validId) {
+    return response.status(400).json({ error: 'Invalid UUID format' });
+  }
+
+  user = users.find((user) => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: 'User account not found' });
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found' });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const validId = validate(id);
+
+  if (!validId) {
+    return response.status(400).json({ error: 'Invalid UUID format' });
+  }
+
+  const user = users.find((user) => user.id == id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User account not found' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
